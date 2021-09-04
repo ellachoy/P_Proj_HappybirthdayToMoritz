fa2af0f1c5da1e64abbf02622f916ebb17730b9f
Ella Choy <egal21de@gmail.com>
2021-09-05 01:42:50 +0200

    correc

---
+++ assets/img/Document_files/main.js
@@ -0,0 +1,12 @@
+console.log('hi');
+
+    function rechnen(name,age ){
+        name= document.getElementById("name");
+        age= age = document.getElementById("age");
+ 
+       document.getElementById("result").innerHTML=`HAllo ${name.value}, Alles gute zum Geburtstag!!! <br> Möchtest du ein Tolles Geschenk von Onkel Sangi bekommen?  Wenn ja, dann clicke den unteren Start button. `;
+       document.getElementById("start").style.display = 'block';
+           
+    }
+
+   
