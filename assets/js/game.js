b90c747fa1a8a28416fae8fe4efcff7a07cc3859
Ella Choy <egal21de@gmail.com>
2021-09-05 01:20:30 +0200

    game

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,16 +9,16 @@ function get_results(){
     com_cal= Math.floor(Math.random()*3);
     console.log(`com : ${com_cal}`);
     str_Input= ['Rock', 'Paper', 'Scissors'];
-    str_Out= (` User:  (${str_Input[user]})  VS  Computer : (${str_Input[com_cal]}) =`);
+    str_Out= (` Moritz:  (${str_Input[user]})  VS  Sangi: (${str_Input[com_cal]}) =`);
     // user=str_Input[user];
     // console.log(user)
 
     if(user== com_cal){
         str_Out = str_Out + "draw!"
     }else if ((user == 0 && com_cal == 1) || (user == 1 && com_cal == 2)|| (user == 2 && com_cal == 0)){
-        str_Out = str_Out + "Computer win!"
+        str_Out = str_Out + "Sangi win!"
     }else{
-        str_Out = str_Out + "User win"
+        str_Out = str_Out + "Moritz win"
     }
      document.getElementById('result').innerHTML =str_Out; 
      document.getElementById('user').innerHTML=str_Input[user]; 
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ console.log('hi');
         name= document.getElementById("name");
         age= age = document.getElementById("age");
  
-       document.getElementById("result").innerHTML=`HAllo ${name.value}, Alles gute zum Geburtstag!!! <br> Möchtest du ein Tooles Geschenk von Onkel Sangi bekommen? . `;
+       document.getElementById("result").innerHTML=`HAllo ${name.value}, Alles gute zum Geburtstag!!! <br> Möchtest du ein Tolles Geschenk von Onkel Sangi bekommen?  Wenn ja, dann clicke den unteren Start button. `;
+       document.getElementById("start").style.display = 'block';
            
     }
 
