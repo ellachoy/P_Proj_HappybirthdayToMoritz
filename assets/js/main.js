70136953b9178330cfc0bb9d934c287cb289b326
Ella Choy <egal21de@gmail.com>
2021-09-04 22:50:40 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+console.log('hi');
+const size =document.getElementById("size");
+const age = document.getElementById("age");
+const breite = document.getElementById("breite")
+
+    function rechnen(){
+        // console.log("rechnen");
+       let num_Size=Number(size.value);// werte von size
+       let num_Age=Number(age.value);// werte von age
+
+
+
+        if (breite.checked){
+            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${breiteKoerper(num_Size, num_Age)}`;
+           
+
+        }else{
+            document.getElementById("result").innerHTML=`Dein Idealgewicht ist:${schmaleKoerper(num_Size, num_Age)}`;
+        }
+    }
+
+   function schmaleKoerper(size,age){
+       return (size - 100) + (age / 10) * 0.9 * 0.9;
+   }
+//    schmaleKoerper();
+   function breiteKoerper(size, age){
+    return (size - 100) + (age / 10) * 0.9 * 1.1;
+   }
+
+//    rausnehmen
+//    breiteKoerper();
+//    function getSize(){
+//        if(breite.checked){
+//            console.log("breite");
+//        }else{
+//            console.log('schmale');
+//        }
+//    }
+ 
+
+//  ersetzen !!!
+// if(breite.checked) {
+//     let gewicht = breiteKoerper(num_Size, num_Age);
+//     document.getElemnentByID(...).innerHtml = '...' + gewicht
+//   }
